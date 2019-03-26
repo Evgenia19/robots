3a89eb7757e1ca49c87d7749968326b7f1005eb2
Evgenia19 <evgeniadolgaeva@gmail.com>
2019-03-26 23:19:14 +0500

    add class for User

--- robots/src/gui/User.java
+++ robots/src/gui/User.java
@@ -1,4 +1,12 @@
 package gui;
 
 public class User {
+
+    final String userId;
+    final RobotsProgram game;
+
+    public User(String id, RobotsProgram newGame) {
+        this.userId = id;
+        this.game = newGame;
+    }
 }
